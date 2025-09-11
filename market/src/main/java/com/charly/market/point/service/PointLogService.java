2909d67c31b969c4b4175e839849778c64e9a2ff
Jnkgb <pjw0918jw@naver.com>
2025-09-11 18:28:15 +0900

    add pointlog

---
+++ market/src/main/java/com/charly/market/point/service/PointLogService.java
@@ -0,0 +1,9 @@
+package com.charly.market.point.service;
+
+
+
+import com.charly.market.point.model.dto.CreatePointLogRequest;
+
+public interface PointLogService {
+    void Serv(CreatePointLogRequest request);
+}
