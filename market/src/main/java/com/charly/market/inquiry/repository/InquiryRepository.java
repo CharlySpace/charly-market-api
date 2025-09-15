e6441d8aad476cff7a51d8d1f94c8a93ec4e1203
yendlessse <yoohanse@naver.com>
2025-09-15 14:20:31 +0900

    add: 문의사항 조회,검색,삭제

---
+++ market/src/main/java/com/charly/market/inquiry/repository/InquiryRepository.java
@@ -0,0 +1,8 @@
+package com.charly.market.inquiry.repository;
+
+import com.charly.market.inquiry.model.entity.Inquiry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+    Inquiry findByInquiryId(Long inquiryId);
+}
