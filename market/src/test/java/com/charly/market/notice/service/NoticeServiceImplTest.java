0d96ba669053603f3338d09c8e6227d154a2d269
yendlessse <yoohanse@naver.com>
2025-10-13 18:21:00 +0900

    add: notice create테스트

---
+++ market/src/test/java/com/charly/market/notice/service/NoticeServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.charly.market.notice.service;
+
+import com.charly.market.notice.model.dto.CreateNoticeRequest;
+import com.charly.market.notice.model.dto.NoticeResponse;
+import com.charly.market.notice.model.entity.Notice;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class NoticeServiceImplTest {
+
+    @Autowired
+    private NoticeService noticeService;
+
+    @Test
+    public void createNotice() {
+        for (long i = 5; i < 10; i++) {
+            //given
+            CreateNoticeRequest request = new CreateNoticeRequest("제목" + i, "내용" + i, "Y", i);
+
+            noticeService.createNotice(request);
+
+            List<NoticeResponse> noticeResponses = noticeService.findAll();
+            System.out.println(noticeResponses);
+        }
+    }
+}
