27e1dfd31e0140878e9f351dd861fbf0b3521338
kim100000000 <kst980510@gmail.com>
2025-10-15 19:11:41 +0900

    fix : AlarmTemplate insert test

--- market/src/main/java/com/charly/market/alarm_template/service/AlarmTemplateService.java
+++ market/src/main/java/com/charly/market/alarm_template/service/AlarmTemplateService.java
@@ -3,18 +3,13 @@ package com.charly.market.alarm_template.service;
 import com.charly.market.alarm_template.model.dto.AlarmTemplateChangeContentRequest;
 import com.charly.market.alarm_template.model.dto.AlarmTemplateRequest;
 import com.charly.market.alarm_template.model.dto.AlarmTemplateResponse;
-import com.charly.market.alarm_template.model.entity.AlarmTemplate;
-import com.charly.market.alarmbox.model.dto.AlarmBoxRequest;
-import com.charly.market.alarmbox.model.dto.AlarmBoxResponse;
-import com.charly.market.alarmbox.model.dto.ChangeStatusRequest;
-import com.charly.market.alarmbox.model.entity.AlarmBox;
+import com.charly.market.alarm_template.model.dto.CreateAlarmTemplateRequest;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AlarmTemplateService {
     // 생성
-    void createAlarmTemplate(AlarmTemplateRequest request);
+    void createAlarmTemplate(CreateAlarmTemplateRequest request);
 
     // 조회
     List<AlarmTemplateResponse> findAll();
